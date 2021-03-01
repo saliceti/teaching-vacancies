7bc5717456ca56a592d3f4a7232ab6bde16bfbe7
Nick Romney <nick.romney@digital.education.gov.uk>
2021-03-01 17:32:22 +0000

    Add offline site S3 access

--- terraform/common/s3.tf
+++ terraform/common/s3.tf
@@ -21,3 +21,32 @@ resource "aws_s3_bucket_public_access_block" "db_backups" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+
+data "aws_iam_policy_document" "offline_site_full_access" {
+  statement {
+    actions = [
+      "s3:GetBucketAcl",
+      "s3:GetBucketLocation",
+      "s3:GetObject",
+      "s3:GetObjectAcl",
+      "s3:ListBucket",
+      "s3:PutBucketAcl",
+      "s3:PutObject"
+    ]
+    resources = [
+      "arn:aws:s3:::arn:aws:s3:::tvs-offline/",
+      "arn:aws:s3:::arn:aws:s3:::tvs-offline/*"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "offline_site_full_access" {
+  name   = "offline_site_full_access"
+  policy = data.aws_iam_policy_document.db_backups_in_s3.json
+}
+
+resource "aws_iam_user_policy_attachment" "offline_site_full_access" {
+  user       = aws_iam_user.deploy.name
+  policy_arn = aws_iam_policy.offline_site_full_access.arn
+}
